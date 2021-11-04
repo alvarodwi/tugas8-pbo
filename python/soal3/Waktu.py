808bba1224b46467f2701f73022ae32469f025ba
Alvaro Dwi <varoa.dwi@gmail.com>
2021-11-04 09:16:56 +0700

    fix hitung selisih pada python rusak

--- python/soal3/Waktu.py
+++ python/soal3/Waktu.py
@@ -6,9 +6,11 @@
                             - Rafiansyah Rasyid Wikawang    (140810200049)
     Tanggal               : 1/11/2021
 """
+
+
 class Waktu:
-    def __init__(self,jam = 0, menit = 0, detik = 0):
-        if(jam > 23 or menit > 59 or detik > 59):
+    def __init__(self, jam=0, menit=0, detik=0):
+        if (jam > 23 or menit > 59 or detik > 59):
             print("Kesalahan input waktu!")
             exit(0)
         else:
@@ -18,23 +20,23 @@ class Waktu:
 
     #method
     def setJam(self, j):
-        if(int(j) > 23):
+        if (int(j) > 23):
             print("Kesalahan input waktu!")
             exit(0)
         self.__jam = int(j)
 
-    def setMenit(self,m):
+    def setMenit(self, m):
         if (int(m) > 59):
             print("Kesalahan input waktu!")
             exit(0)
         self.__menit = int(m)
 
     def setDetik(self, d):
-        if(int(d) > 59):
+        if (int(d) > 59):
             print("Kesalahan input waktu!")
             exit(0)
         self.__detik = int(d)
-    
+
     def getJam(self):
         return self.__jam
 
@@ -43,26 +45,27 @@ class Waktu:
 
     def getDetik(self):
         return self.__jam
-    
-    
+
     def hitungSelisih(self, akhir):
         temp = Waktu()
-        temp.__jam = akhir.getJam() - self.__jam
-        temp.__menit = akhir.getMenit() - self.__menit
-        if (temp.getMenit() < 0):
-            temp.__menit = 60 + temp.getMenit()
-            temp.__jam-=1
-        temp.__detik = akhir.getDetik() - self.__detik
-        if (temp.getDetik() < 0):
-            temp.__detik = abs(temp.getDetik())
-            temp.__menit-=1
-            if (temp.getMenit() < 0):
-                temp.__menit = 60 + temp.getMenit()
-                temp.__jam-=1
+        temp.__jam = akhir.__jam - self.__jam
+        temp.__menit = akhir.__menit - self.__menit
+        if (temp.__menit < 0):
+            temp.__menit = 60 + temp.__menit
+            temp.__jam -= 1
+        temp.__detik = akhir.__detik - self.__detik
+        if (temp.__detik < 0):
+            temp.__detik = abs(temp.__detik)
+            temp.__menit -= 1
+            if (temp.__menit < 0):
+                temp.__menit = 60 + temp.__menit
+                temp.__jam -= 1
         return temp
 
     def toString(self):
-        return str(self.__jam) + ":" + str(self.__menit) + ":" + str(self.__detik)
+        return str(self.__jam) + ":" + str(self.__menit) + ":" + str(
+            self.__detik)
 
     def printWaktu(self):
-        return str(self.__jam) + " jam " + str(self.__menit) + " menit " + str(self.__detik) + " detik"
+        return str(self.__jam) + " jam " + str(self.__menit) + " menit " + str(
+            self.__detik) + " detik"
